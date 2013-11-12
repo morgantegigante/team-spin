217da13c7a72d1fb2703194c7a74aca26be26f31
Morganne Gagne <morgannegagne@gmail.com>
2013-11-12 13:40:12 -0500

    bidirectional spinning and wireless led code

---
+++ Bidirectional_Spinning.ino
@@ -0,0 +1,41 @@
+// This code should read the state of the buttons and turn the motor on/off and c/c-c
+
+// Set up pin numbers
+const int forwardbuttonPin = 2;
+const int reversebuttonPin = 3;
+const int forwardrelayPin = 4;
+const int reverserelayPin = 5;
+
+// Variable for the motor states
+int forwardmotorState = 0;
+int reversemotorState = 0;
+
+void setup() {
+  // Initalize inputs and outputs
+  pinMode(forwardbuttonPin, INPUT);
+  pinMode(reversebuttonPin, INPUT);
+  pinMode(forwardrelayPin, OUTPUT);
+  pinMode(reverserelayPin, OUTPUT);
+}
+
+void loop(){
+  // Read the state of the button
+  forwardmotorState = digitalRead(forwardbuttonPin);
+  reversemotorState = digitalRead(reversebuttonPin);
+  
+  // Check which button is pressed
+  if (forwardmotorState == HIGH) {
+   // Turns one relay on
+  digitalWrite(forwardrelayPin, HIGH);
+  }
+  else if (reversemotorState == HIGH) {
+    digitalWrite(forwardrelayPin, HIGH);
+    digitalWrite(reverserelayPin, HIGH);
+  }
+  else {
+    // Turns the motor off
+    digitalWrite(forwardrelayPin, LOW);
+    digitalWrite(reverserelayPin, LOW);
+  }
+}
+    
